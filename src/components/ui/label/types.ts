e1b13aa41b00ab370e2a0407c5a48743aea70fad
lucas dalan <lucasdalan09@gmail.com>
2025-06-05 13:49:22 -0300

    feat: added form

---
+++ src/components/ui/label/types.ts
@@ -0,0 +1,4 @@
+import { ComponentProps } from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+
+export type LabelProps = ComponentProps<typeof LabelPrimitive.Root>;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const uppercaseFirstLetter = (str: string) =>
+  `${str.substring(0, 1).toUpperCase()}${str.substring(1)}`;
